14f5cca70cbe06bbd1ea62d74b4f077f81031bc7
nisihiramorinobu <84651061+nisihiramorinobu@users.noreply.github.com>
2021-07-03 19:05:52 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+{
+  
+  const h1 = document.querySelector('h1');
+  h1.textContent = window.innerWidth;
+  const p = document.querySelector('p');
+  p.textContent = window.innerHeight;
+}
